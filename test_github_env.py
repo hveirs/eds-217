5b96588b206e56469e94bf40f876e37fae524b84
hveirs <hveirs3@outlook.com>
2022-09-06 16:49:00 -0700

    added day 6 tasks

---
+++ test_github_env.py
@@ -0,0 +1,7 @@
+# Test environment
+# Hello! This is a test to see if github works with VS
+test_variable = 34
+test_variable2 = test_variable + 12
+
+print(test_variable2)
+# test_variable2 should be = 46
